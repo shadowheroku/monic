d555179427e88d20d4704a85d78e63ae2de42b8d
shadowheroku <shadowheroku@gmail.com>
2025-08-17 00:44:56 +0530

    Update __init__.py

--- misskaty/core/misskaty_patch/__init__.py
+++ misskaty/core/misskaty_patch/__init__.py
@@ -1,15 +1,28 @@
 from . import bound, decorators, methods
 
-# --- Add MongoDB patch for Pyrogram Client ---
+# --- Add MongoDB & compatibility patch for Pyrogram Client ---
 from pyrogram import Client as PyroClient
 
 # Backup original __init__
 _original_init = PyroClient.__init__
 
+
 def patched_init(self, *args, **kwargs):
-    # Handle mongodb argument if provided
-    self.mongodb = kwargs.pop("mongodb", None)
+    # MissKaty fork passed extra arguments not supported in official Pyrogram.
+    # We just absorb & ignore them.
+    unsupported_keys = [
+        "mongodb",
+        "max_concurrent_transmissions",  # MissKaty custom
+        "max_concurrent_transmissions_limit",  # just in case
+    ]
+
+    for key in unsupported_keys:
+        if key in kwargs:
+            setattr(self, key, kwargs.pop(key, None))
+
+    # Call the original Pyrogram Client.__init__
     _original_init(self, *args, **kwargs)
+
 
 # Patch Pyrogram Client
 PyroClient.__init__ = patched_init
