b03b74d8da7d931092315a07372e3d488489a8f8
shadowheroku <shadowheroku@gmail.com>
2025-08-17 00:33:20 +0530

    Update __init__.py

--- misskaty/core/misskaty_patch/__init__.py
+++ misskaty/core/misskaty_patch/__init__.py
@@ -1,1 +1,15 @@
 from . import bound, decorators, methods
+
+# --- Add MongoDB patch for Pyrogram Client ---
+from pyrogram import Client as PyroClient
+
+# Backup original __init__
+_original_init = PyroClient.__init__
+
+def patched_init(self, *args, **kwargs):
+    # Handle mongodb argument if provided
+    self.mongodb = kwargs.pop("mongodb", None)
+    _original_init(self, *args, **kwargs)
+
+# Patch Pyrogram Client
+PyroClient.__init__ = patched_init
